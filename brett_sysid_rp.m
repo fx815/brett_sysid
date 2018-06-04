34fea0f146fda08a6b5ae8acb81c42d57fdb469a
BrettRStephens <sbrett@student.ethz.ch>
2018-06-04 13:44:50 +0100

    initial commit

---
+++ brett_sysid_rp.m
@@ -0,0 +1,343 @@
+%% load bag files: one bag for estimating the TF, one for validating that TF
+clear;
+close all
+clc;
+path(path,'../read_bags');
+path(path,'../helper_functions');
+
+bag_name1 = 'rp1_2018-05-21-15-40-31.bag';
+bag_name2 = 'rp2_2018-05-21-15-45-20.bag';
+
+bag1 = ros.Bag(bag_name1);
+bag2 = ros.Bag(bag_name2);
+
+%% read topics
+imu_data1 = readImu(bag1, '/mavros/imu/data');
+imu_data2 = readImu(bag2, '/mavros/imu/data');
+
+attitude_cmd1 = readAttitudeTarget(bag1, '/mavros/setpoint_raw/target_attitude');
+attitude_cmd2 = readAttitudeTarget(bag2, '/mavros/setpoint_raw/target_attitude');
+
+attitude_cmd1.rpy = quat2rpy([attitude_cmd1.q(4,:)', attitude_cmd1.q(1:3,:)']');
+attitude_cmd2.rpy = quat2rpy([attitude_cmd2.q(4,:)', attitude_cmd2.q(1:3,:)']');
+
+imu_data1.rpy = quat2rpy([imu_data1.q(4,:)', imu_data1.q(1:3,:)']');
+imu_data2.rpy = quat2rpy([imu_data2.q(4,:)', imu_data2.q(1:3,:)']');
+
+attitude_cmd1.rpy = quat2rpy([attitude_cmd1.q(4,:)', attitude_cmd1.q(1:3,:)']');
+attitude_cmd2.rpy = quat2rpy([attitude_cmd2.q(4,:)', attitude_cmd2.q(1:3,:)']');
+
+t_start1 = imu_data1.t(1);
+t_start2 = imu_data2.t(1);
+
+imu_data1.t = imu_data1.t - t_start1;
+imu_data2.t = imu_data2.t - t_start2;
+
+attitude_cmd1.t = attitude_cmd1.t - attitude_cmd1.t(1);
+attitude_cmd2.t = attitude_cmd2.t - attitude_cmd2.t(1);
+
+%% plot
+% figure(1);
+% subplot(2,1,1);
+% ax = axes;
+% plot(imu_data1.t, imu_data1.rpy(1,:), 'linewidth', 2);
+% hold on;
+% plot(attitude_cmd1.t, attitude_cmd1.rpy(1,:), '--', 'linewidth', 2);
+% xlabel('time');
+% ylabel('roll [rad]');
+% title('dataset 1 roll angle');
+% legend('\phi imu', '\phi cmd');
+% grid on;
+% ax.FontSize = 16;
+% 
+% subplot(2,1,2);
+% ax = axes;
+% plot(imu_data2.t, imu_data2.rpy(1,:), 'linewidth', 2);
+% hold on;
+% plot(attitude_cmd2.t, attitude_cmd2.rpy(1,:), '--', 'linewidth', 2);
+% xlabel('time');
+% ylabel('roll [rad]');
+% title('dataset 2 roll angle');
+% legend('\phi imu', '\phi cmd');
+% grid on;
+% ax.FontSize = 16;
+% 
+% figure(2);
+% subplot(2,1,1);
+% ax = axes;
+% plot(imu_data1.t, imu_data1.rpy(2,:), 'linewidth', 2);
+% hold on;
+% plot(attitude_cmd1.t, attitude_cmd1.rpy(2,:), '--', 'linewidth', 2);
+% xlabel('time');
+% ylabel('\theta [rad]');
+% title('datset 1 pitch angle');
+% legend('\theta imu', '\theta cmd');
+% grid on;
+% ax.FontSize = 16;
+% 
+% subplot(2,1,2);
+% ax = axes;
+% plot(imu_data2.t, imu_data2.rpy(2,:), 'linewidth', 2);
+% hold on;
+% plot(attitude_cmd2.t, attitude_cmd2.rpy(2,:), '--', 'linewidth', 2);
+% xlabel('time');
+% ylabel('\theta [rad]');
+% title('datset 2 pitch angle');
+% legend('\theta imu', '\theta cmd');
+% grid on;
+% ax.FontSize = 16;
+
+%% sysid begins
+attitude_cmd1.rpy_interp = zeros(size(imu_data1.rpy));
+attitude_cmd2.rpy_interp = zeros(size(imu_data2.rpy));
+
+%interp roll
+attitude_cmd1.rpy_interp(1,:) = interp1(attitude_cmd1.t, attitude_cmd1.rpy(1,:), imu_data1.t, 'spline');
+attitude_cmd2.rpy_interp(1,:) = interp1(attitude_cmd2.t, attitude_cmd2.rpy(1,:), imu_data2.t, 'spline');
+
+%interp pitch
+attitude_cmd1.rpy_interp(2,:) = interp1(attitude_cmd1.t, attitude_cmd1.rpy(2,:), imu_data1.t, 'spline');
+attitude_cmd2.rpy_interp(2,:) = interp1(attitude_cmd2.t, attitude_cmd2.rpy(2,:), imu_data2.t, 'spline');
+
+attitude_cmd1.t = imu_data1.t;
+attitude_cmd2.t = imu_data2.t;
+
+%get rid of first and last x seconds (to remove ground and transient effects)
+st = 10;
+clip = 10;
+
+imu_data1.t = imu_data1.t(imu_data1.t > st & imu_data1.t < imu_data1.t(end)-clip);
+imu_data2.t = imu_data2.t(imu_data2.t > st & imu_data2.t < imu_data2.t(end)-clip);
+
+imu_data1.rpy = imu_data1.rpy(:, imu_data1.t > st & imu_data1.t < imu_data1.t(end)-clip);
+imu_data2.rpy = imu_data2.rpy(:, imu_data2.t > st & imu_data2.t < imu_data2.t(end)-clip);
+
+attitude_cmd1.t = attitude_cmd1.t(attitude_cmd1.t > st & attitude_cmd1.t < attitude_cmd1.t(end)-clip);
+attitude_cmd2.t = attitude_cmd2.t(attitude_cmd2.t > st & attitude_cmd2.t < attitude_cmd2.t(end)-clip);
+
+attitude_cmd1.rpy_interp = attitude_cmd1.rpy_interp(:, attitude_cmd1.t > st & attitude_cmd1.t < attitude_cmd1.t(end)-clip);
+attitude_cmd2.rpy_interp = attitude_cmd2.rpy_interp(:, attitude_cmd2.t > st & attitude_cmd2.t < attitude_cmd2.t(end)-clip);
+
+
+%% ID of roll system
+delay=[]; NaN;
+np = 2; %1 for 1st order system, 2 for 2nd order system
+
+%experiment 1
+Experiment1.u1 = attitude_cmd1.rpy_interp(1,:);
+Experiment1.y1 = imu_data1.rpy(1,:);
+
+dt1 = mean(diff(imu_data1.t));
+
+roll_data1 = iddata(Experiment1.y1',Experiment1.u1',dt1, ...
+    'ExperimentName', 'SysID_1', 'InputName','roll_{cmd}', ...
+    'OutputName','roll', 'InputUnit','rad', 'OutputUnit','rad', ...
+'TimeUnit','Second');
+roll_data1 = detrend(roll_data1);
+
+%experiment 2
+Experiment2.u1 = attitude_cmd2.rpy_interp(1,:);
+Experiment2.y1 = imu_data2.rpy(1,:);
+
+dt2 = mean(diff(imu_data2.t));
+
+roll_data2 = iddata(Experiment2.y1',Experiment2.u1',dt2,...
+    'ExperimentName', 'SysID_2', 'InputName','roll_{cmd}',...
+    'OutputName','roll', 'InputUnit','rad', 'OutputUnit','rad',...
+'TimeUnit','Second');
+roll_data2 = detrend(roll_data2);
+
+% *At this point we have 3 options!*
+% 
+% # Estimate a model from both experiments - but cannot validate it on independent dataset
+% # Estimate a model from Exp1 and validate it on data from Exp2
+% # Estimate a model from Exp2 and validate it on data from Exp1
+%For now we choose the best model from options 2 and 3
+
+
+%Assume 1st  order system  
+%np = 2;
+nz = 0;
+
+%Generate model using Experiment1 and validate the model with Experiment2
+roll_estimated_tf1 = tfest(roll_data1,np, nz,delay);
+
+[~, fit1, ~] = compare(roll_data2, roll_estimated_tf1);
+
+%Generate model using Experiment2 and validate the model with Experiment1
+roll_estimated_tf2 = tfest(roll_data2,np, nz,delay);
+
+[~, fit2, ~] = compare(roll_data1, roll_estimated_tf2);
+
+if fit1>fit2
+    %We pick the first Identification
+    roll_estimated_tf = roll_estimated_tf1;
+    disp('The roll model is estimated using experiment 1 and validated on data from experiment 2');
+    figure;
+    compare(roll_data2, roll_estimated_tf1);    
+    disp(strcat('The roll model fits the validation data with **',...
+        num2str(fit1), '** %'));
+else
+    %We pick the second Identification
+    roll_estimated_tf = roll_estimated_tf2;
+    disp('The roll model is estimated using experiment 2 and validated on data from experiment 1');
+    figure;
+    compare(roll_data1, roll_estimated_tf2);
+    disp(strcat('The roll model fits the validation data with **',...
+        num2str(fit2), '** %'));
+end
+
+
+%% ID pitch of system
+
+%experiment 1
+Experiment1.u2 = attitude_cmd1.rpy_interp(2,:);
+Experiment1.y2 = imu_data1.rpy(2,:);
+
+pitch_data1 = iddata(Experiment1.y2',Experiment1.u2',dt1,...
+    'ExperimentName', 'SysID_1', 'InputName','pitch_{cmd}',...
+    'OutputName','pitch', 'InputUnit','rad', 'OutputUnit','rad',...
+'TimeUnit','Second');
+
+pitch_data1 = detrend(pitch_data1);
+
+%experiment 2
+Experiment2.u2 = attitude_cmd2.rpy_interp(2,:);
+Experiment2.y2 = imu_data2.rpy(2,:);
+
+pitch_data2 = iddata(Experiment2.y2',Experiment2.u2',dt2, ...
+    'ExperimentName', 'SysID_2', 'InputName','pitch_{cmd}',...
+    'OutputName','pitch', 'InputUnit','rad', 'OutputUnit','rad', ...
+    'TimeUnit','Second');
+
+pitch_data2 = detrend(pitch_data2); 
+
+% *At this point we have 3 options!*
+% 
+% # Estimate a model from both experiments - but cannot validate it on independent dataset
+% # Estimate a model from Exp1 and validate it on data from Exp2
+% # Estimate a model from Exp2 and validate it on data from Exp1
+%For now we choose the best model from options 2 and 3
+  
+%Assume 1st order system
+%np = 2;
+nz = 0;
+
+%Generate model using Experiment1 and validate the model with Experiment2
+pitch_estimated_tf1 = tfest(pitch_data1,np, nz,delay);
+
+[~, fit1, ~] = compare(pitch_data2, pitch_estimated_tf1);
+
+%Generate model using Experiment2 and validate the model with Experiment1
+pitch_estimated_tf2 = tfest(pitch_data2,np, nz,delay);
+
+[~, fit2, ~] = compare(pitch_data1, pitch_estimated_tf2);
+
+if fit1>fit2
+    %We pick the first Identification
+    pitch_estimated_tf = pitch_estimated_tf1;
+    disp('The pitch model is estimated using experiment 1 and validated on data from experiment 2');
+    figure;
+    compare(pitch_data2, pitch_estimated_tf1);
+    disp(strcat('The pitch model fits the validation data with **', ...
+        num2str(fit1), '** %'));
+else
+    %We pick the second Identification
+    pitch_estimated_tf = pitch_estimated_tf2;
+    disp('The pitch model is estimated using experiment 2 and validated on data from experiment 1');
+    figure;
+    compare(pitch_data1, pitch_estimated_tf2);
+    disp(strcat('The pitch model fits the validation data with **', ...
+        num2str(fit2), '** %'));
+end
+
+%% Estimate the Whole System as 2-input 2-output MIMO System
+% *The purpose here is to see of there is coupling*
+
+dt2 = dt1;    
+Data1 = iddata([Experiment1.y1', Experiment1.y2'], ...
+    [Experiment1.u1', Experiment1.u2'], dt1, ...
+    'ExperimentName', 'FireFlySysID_1', ...
+    'InputName',{'roll_{cmd}','pitch_{cmd}'},...
+    'OutputName',{'roll','pitch'}', ...
+    'InputUnit',{'rad', 'rad'},...
+    'OutputUnit',{'rad', 'rad'},...
+    'TimeUnit','Second');
+
+Data2 = iddata([Experiment2.y1', Experiment2.y2'], ...
+    [Experiment2.u1', Experiment2.u2'], dt2, ...
+    'ExperimentName', 'FireFlySysID_2', ...
+    'InputName',{'roll_{cmd}','pitch_{cmd}'},...
+    'OutputName',{'roll','pitch'}', ...
+    'InputUnit',{'rad', 'rad'},...
+    'OutputUnit',{'rad', 'rad'}, ...
+    'TimeUnit','Second');
+
+MergedData = merge(Data1, Data2);
+
+%np = 2;
+nz = 0;
+Full_estimated_tf = tfest(MergedData, np,nz);
+
+figure;
+bodemag(Full_estimated_tf);
+
+%%% Estimated Transfer Functions
+
+disp('Roll estimated transfer function is: ');
+tf(roll_estimated_tf)
+
+if(np==1)
+    roll_params=getpvec(roll_estimated_tf);
+    roll_gain=roll_params(1)/roll_params(2);
+    roll_tau=1/roll_params(2);
+    fprintf('roll tau=%.3f, gain=%.3f\n',roll_tau,roll_gain);
+elseif(np==2)
+    roll_params=getpvec(roll_estimated_tf);
+    roll_omega=sqrt(roll_params(3));
+    roll_gain=roll_params(1)/roll_params(3);
+    roll_damping=roll_params(2)/(2*roll_omega);
+    fprintf('roll omega=%.3f, gain=%.3f damping=%.3f\n',roll_omega,roll_gain,roll_damping);
+end
+
+figure('Name','System analysis (roll)');
+subplot(311);
+bode(roll_estimated_tf); grid;
+title('Roll bode plot');
+
+subplot(312);
+rlocusplot(roll_estimated_tf); grid;
+title('Roll RootLucas plot');
+
+subplot(313);
+step(roll_estimated_tf); grid;
+title('Roll step response plot');
+
+disp('Pitch estimated transfer function is: ');
+tf(pitch_estimated_tf)
+
+if(np==1)
+    pitch_params=getpvec(pitch_estimated_tf);
+    pitch_gain=pitch_params(1)/pitch_params(2);
+    pitch_tau=1/pitch_params(2);
+    fprintf('pitch tau=%.3f, gain=%.3f\n',pitch_tau, pitch_gain);
+elseif(np==2)
+    pitch_params=getpvec(pitch_estimated_tf);
+    pitch_omega=sqrt(pitch_params(3));
+    pitch_gain=pitch_params(1)/pitch_params(3);
+    pitch_damping=pitch_params(2)/(2*pitch_omega);
+    fprintf('pitch omega=%.3f, gain=%.3f damping=%.3f\n',pitch_omega,pitch_gain,pitch_damping);
+end
+
+figure('Name','System analysis (pitch)');
+subplot(311);
+bode(pitch_estimated_tf); grid;
+title('Pitch bode plot');
+
+subplot(312);
+rlocusplot(pitch_estimated_tf); grid;
+title('Pitch RootLucas plot');
+
+subplot(313);
+step(pitch_estimated_tf); grid;
+title('Pitch step response plot');
---
+++ brett_sysid_yaw.m
@@ -0,0 +1,163 @@
+%% load bag files: one bag for estimating the TF, one for validating that TF
+clear;
+close all
+clc;
+path(path,'../read_bags');
+path(path,'../helper_functions');
+
+bag_name1 = 'yaw1_2018-05-21-15-48-48.bag';
+bag_name2 = 'yaw2_2018-05-21-15-49-58.bag';
+
+bag1 = ros.Bag(bag_name1);
+bag2 = ros.Bag(bag_name2);
+
+%% read topics
+imu_data1 = readImu(bag1, '/mavros/imu/data');
+imu_data2 = readImu(bag2, '/mavros/imu/data');
+
+attitude_cmd1 = readAttitudeTarget(bag1, '/mavros/setpoint_raw/target_attitude');
+attitude_cmd2 = readAttitudeTarget(bag2, '/mavros/setpoint_raw/target_attitude');
+
+attitude_cmd1.rpy = quat2rpy([attitude_cmd1.q(4,:)', attitude_cmd1.q(1:3,:)']');
+attitude_cmd2.rpy = quat2rpy([attitude_cmd2.q(4,:)', attitude_cmd2.q(1:3,:)']');
+
+imu_data1.rpy = quat2rpy([imu_data1.q(4,:)', imu_data1.q(1:3,:)']');
+imu_data2.rpy = quat2rpy([imu_data2.q(4,:)', imu_data2.q(1:3,:)']');
+
+attitude_cmd1.rpy = quat2rpy([attitude_cmd1.q(4,:)', attitude_cmd1.q(1:3,:)']');
+attitude_cmd2.rpy = quat2rpy([attitude_cmd2.q(4,:)', attitude_cmd2.q(1:3,:)']');
+
+t_start1 = imu_data1.t(1);
+t_start2 = imu_data2.t(1);
+
+imu_data1.t = imu_data1.t - t_start1;
+imu_data2.t = imu_data2.t - t_start2;
+
+attitude_cmd1.t = attitude_cmd1.t - attitude_cmd1.t(1);
+attitude_cmd2.t = attitude_cmd2.t - attitude_cmd2.t(1);
+
+%% sysid begins
+attitude_cmd1.y_interp = zeros(length(imu_data1.rpy));
+attitude_cmd2.y_interp = zeros(length(imu_data2.rpy));
+
+%interp yaw
+attitude_cmd1.y_interp = interp1(attitude_cmd1.t, attitude_cmd1.rpy(3,:), imu_data1.t, 'spline');
+attitude_cmd2.y_interp = interp1(attitude_cmd2.t, attitude_cmd2.rpy(3,:), imu_data2.t, 'spline');
+
+attitude_cmd1.t = imu_data1.t;
+attitude_cmd2.t = imu_data2.t;
+
+%get rid of first and last x seconds (to remove ground and transient effects)
+st = 15;
+clip = 20;
+
+imu_data1.t = imu_data1.t(imu_data1.t > st & imu_data1.t < imu_data1.t(end)-clip);
+imu_data2.t = imu_data2.t(imu_data2.t > st & imu_data2.t < imu_data2.t(end)-clip);
+
+imu_data1.rpy = imu_data1.rpy(:, imu_data1.t > st & imu_data1.t < imu_data1.t(end)-clip);
+imu_data2.rpy = imu_data2.rpy(:, imu_data2.t > st & imu_data2.t < imu_data2.t(end)-clip);
+
+attitude_cmd1.t = attitude_cmd1.t(attitude_cmd1.t > st & attitude_cmd1.t < attitude_cmd1.t(end)-clip);
+attitude_cmd2.t = attitude_cmd2.t(attitude_cmd2.t > st & attitude_cmd2.t < attitude_cmd2.t(end)-clip);
+
+attitude_cmd1.y_interp = attitude_cmd1.y_interp(:, attitude_cmd1.t > st & attitude_cmd1.t < attitude_cmd1.t(end)-clip);
+attitude_cmd2.y_interp = attitude_cmd2.y_interp(:, attitude_cmd2.t > st & attitude_cmd2.t < attitude_cmd2.t(end)-clip);
+
+
+%% ID of yaw system
+np = 2; %1 for 1st order system, 2 for 2nd order system
+
+%experiment 1
+Experiment1.u1 = attitude_cmd1.y_interp;
+Experiment1.y1 = imu_data1.rpy(3,:);
+
+dt1 = mean(diff(imu_data1.t));
+
+yaw_data1 = iddata(Experiment1.y1',Experiment1.u1',dt1, ...
+    'ExperimentName', 'SysID_1', 'InputName','yaw_{cmd}', ...
+    'OutputName','yaw', 'InputUnit','rad', 'OutputUnit','rad', ...
+'TimeUnit','Second');
+yaw_data1 = detrend(yaw_data1);
+
+%experiment 2
+Experiment2.u1 = attitude_cmd2.y_interp;
+Experiment2.y1 = imu_data2.rpy(3,:);
+
+dt2 = mean(diff(imu_data2.t));
+
+yaw_data2 = iddata(Experiment2.y1',Experiment2.u1',dt2,...
+    'ExperimentName', 'SysID_2', 'InputName','yaw_{cmd}',...
+    'OutputName','yaw', 'InputUnit','rad', 'OutputUnit','rad',...
+'TimeUnit','Second');
+yaw_data2 = detrend(yaw_data2);
+
+% *At this point we have 3 options!*
+% 
+% # Estimate a model from both experiments - but cannot validate it on independent dataset
+% # Estimate a model from Exp1 and validate it on data from Exp2
+% # Estimate a model from Exp2 and validate it on data from Exp1
+%For now we choose the best model from options 2 and 3
+
+
+%Assume 1st  order system  
+%np = 2;
+nz = 0;
+
+%Generate model using Experiment1 and validate the model with Experiment2
+yaw_estimated_tf1 = tfest(yaw_data1,np, nz);
+
+[~, fit1, ~] = compare(yaw_data2, yaw_estimated_tf1);
+
+%Generate model using Experiment2 and validate the model with Experiment1
+yaw_estimated_tf2 = tfest(yaw_data2,np, nz);
+
+[~, fit2, ~] = compare(yaw_data1, yaw_estimated_tf2);
+
+if fit1>fit2
+    %We pick the first Identification
+    yaw_estimated_tf = yaw_estimated_tf1;
+    disp('The yaw is estimated using experiment 1 and validated on data from experiment 2');
+    figure;
+    compare(yaw_data2, yaw_estimated_tf1);    
+    disp(strcat('The yaw model fits the validation data with **',...
+        num2str(fit1), '** %'));
+else
+    %We pick the second Identification
+    yaw_estimated_tf = yaw_estimated_tf2;
+    disp('The yaw model is estimated using experiment 2 and validated on data from experiment 1');
+    figure;
+    compare(yaw_data1, yaw_estimated_tf2);
+    disp(strcat('The yaw model fits the validation data with **',...
+        num2str(fit2), '** %'));
+end
+
+%%% Estimated Transfer Functions
+
+disp('yawrate estimated transfer function is: ');
+tf(yaw_estimated_tf)
+yawrate_params=getpvec(yaw_estimated_tf);
+if(np==1)
+    yawrate_gain=yawrate_params(1)/yawrate_params(2);
+    yawrate_tau=1/yawrate_params(2);
+    fprintf('yawrate gain=%.3f, tau=%.3f\n',yawrate_gain,yawrate_tau);
+elseif(np==2)
+    yawrate_omega=sqrt(yawrate_params(3));
+    yawrate_gain=yawrate_params(1)/yawrate_params(3);
+    yawrate_damping=yawrate_params(2)/(2*yawrate_omega);
+    fprintf('yawrate omega=%.3f, gain=%.3f damping=%.3f\n',yawrate_omega,yawrate_gain,yawrate_damping);
+end
+
+
+figure('Name','System analysis (yawrate)');
+subplot(311);
+bode(yaw_estimated_tf); grid;
+title('yawrate bode plot');
+
+subplot(312);
+rlocusplot(yaw_estimated_tf); grid;
+title('yawrate RootLucas plot');
+
+subplot(313);
+step(yaw_estimated_tf); grid;
+title('yawrate step response plot');
+
